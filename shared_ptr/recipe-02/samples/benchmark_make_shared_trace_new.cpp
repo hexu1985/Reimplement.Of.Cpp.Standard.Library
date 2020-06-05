07ca7e1360904fd62ad56a50f102e3c6594fde01
hexu <hexu@sensetime.com>
2020-06-05 15:48:37 +0800

    add benchmark_make_shared_trace_new.cpp and sample_make_shared_trace_new.cpp

---
+++ shared_ptr/recipe-02/samples/benchmark_make_shared_trace_new.cpp
@@ -0,0 +1,41 @@
+// make_shared example
+#include <iostream>
+#include <memory>
+
+using std::shared_ptr;
+using std::make_shared;
+
+struct TestClass {
+    TestClass(int i) {}
+
+    uint64_t array[10];
+};
+
+void *operator new(size_t sz)
+{
+    void *res = malloc(sz);
+    if (res == 0) {
+        printf("no memory");
+        throw std::bad_alloc();
+    }
+    printf("\n*** new %d bytes at %p ***\n", (int) sz, res);
+    return res;
+}
+
+void operator delete(void *ptr)
+{
+    printf("\n*** delete at %p ***\n", ptr);
+    free(ptr);
+}
+
+int main () {
+
+    printf("shared_ptr<TestClass> foo = make_shared<TestClass> (10);\n");
+    shared_ptr<TestClass> foo = make_shared<TestClass> (10);
+
+    printf("\n\n");
+    printf("shared_ptr<TestClass> foo2 (new TestClass(10));\n");
+    shared_ptr<TestClass> foo2 (new TestClass(10));
+
+    return 0;
+}
