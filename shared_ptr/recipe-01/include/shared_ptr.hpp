9db1c7017a87077ec3081a2304549dede0806a40
hexu <hexu_1985@sina.com>
2020-11-26 21:22:28 +0800

    fix compile error of shared_ptr.hpp

--- shared_ptr/recipe-01/include/shared_ptr.hpp
+++ shared_ptr/recipe-01/include/shared_ptr.hpp
@@ -41,7 +41,7 @@ public:
      * @brief 构造无被管理对象的shared_ptr, 即空shared_ptr
      */
     shared_ptr() {}
-    shared_ptr(nullptr_t) {}
+    shared_ptr(std::nullptr_t) {}
 
     /**
      * @brief 构造shared_ptr, 管理ptr所指向的对象.
