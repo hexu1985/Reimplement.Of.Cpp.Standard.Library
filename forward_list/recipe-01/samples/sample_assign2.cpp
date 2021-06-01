0eb62df9b5d4e08123b91f44cf664c9c55568930
hexu <hexu_1985@sina.com>
2021-06-01 17:39:45 +0800

    add assign and operator = samples of forward_list

---
+++ forward_list/recipe-01/samples/sample_assign2.cpp
@@ -0,0 +1,33 @@
+// forward_list::assign
+#include <iostream>
+#include "forward_list.hpp"
+
+int main ()
+{
+  mini_stl::forward_list<int> first;
+  mini_stl::forward_list<int> second;
+
+  first.assign (4,15);                           // 15 15 15 15
+
+  second.assign (first.begin(),first.end());     // 15 15 15 15
+
+  first.assign ( {77, 2, 16} );                  // 77 2 16
+
+  std::cout << "first contains: ";
+  for (int& x : first) std::cout << ' ' << x;
+  std::cout << '\n';
+
+  std::cout << "second contains: ";
+  for (int& x : second) std::cout << ' ' << x;
+  std::cout << '\n';
+
+  return 0;
+}
+
+/*
+Output:
+
+first contains: 77 2 16
+second contains: 15 15 15 15
+*/
+
---
+++ forward_list/recipe-01/samples/sample_operator_assign2.cpp
@@ -0,0 +1,45 @@
+#include "forward_list.hpp"
+#include <iostream>
+ 
+void display_sizes(const mini_stl::forward_list<int>& nums1,
+                   const mini_stl::forward_list<int>& nums2,
+                   const mini_stl::forward_list<int>& nums3)
+{
+    std::cout << "nums1: " << std::distance(nums1.begin(), nums1.end())
+              << " nums2: " << std::distance(nums2.begin(), nums2.end())
+              << " nums3: " << std::distance(nums3.begin(), nums3.end()) << '\n';
+}
+ 
+int main()
+{
+    mini_stl::forward_list<int> nums1 {3, 1, 4, 6, 5, 9};
+    mini_stl::forward_list<int> nums2; 
+    mini_stl::forward_list<int> nums3;
+ 
+    std::cout << "Initially:\n";
+    display_sizes(nums1, nums2, nums3);
+ 
+    // copy assignment copies data from nums1 to nums2
+    nums2 = nums1;
+ 
+    std::cout << "After assigment:\n"; 
+    display_sizes(nums1, nums2, nums3);
+ 
+    // move assignment moves data from nums1 to nums3,
+    // modifying both nums1 and nums3
+    nums3 = std::move(nums1);
+ 
+    std::cout << "After move assigment:\n"; 
+    display_sizes(nums1, nums2, nums3);
+}
+
+/*
+Output:
+
+Initially:
+nums1: 6 nums2: 0 nums3: 0
+After assigment:
+nums1: 6 nums2: 6 nums3: 0
+After move assigment:
+nums1: 0 nums2: 6 nums3: 6
+*/
