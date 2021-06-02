2361fa368f5cdd40ae6619f54b0ad896c828417c
hexu <hexu_1985@sina.com>
2021-06-02 17:47:22 +0800

    调整forward_list.hpp缩进

--- forward_list/recipe-01/include/forward_list_impl.h
+++ forward_list/recipe-01/include/forward_list_impl.h
@@ -26,7 +26,7 @@ namespace single_linked {
  * 
  */
 typedef struct list_node_t {
-	struct list_node_t *next;
+    struct list_node_t *next;
 } list_node_t;
 
 /**
@@ -36,7 +36,7 @@ typedef struct list_node_t {
  *
  */
 typedef struct list_t {
-	list_node_t head;
+    list_node_t head;
 } list_t;
 
 /**
@@ -48,28 +48,28 @@ typedef struct list_t {
 inline
 void list_init(list_t *lst)
 {
-	lst->head.next = NULL;
+    lst->head.next = NULL;
 }
 
 // 返回链表头部第一个节点(非哑元头节点)的指针
 inline
 list_node_t *list_head(const list_t *lst)
 {
-	return (list_node_t *) lst->head.next;
+    return (list_node_t *) lst->head.next;
 }
 
 // 返回链表哑元头节点的指针
 inline
 list_node_t *list_before_head(const list_t *lst)
 {
-	return (list_node_t *) &lst->head;
+    return (list_node_t *) &lst->head;
 }
 
 // 判断链表是否为空
 inline
 int list_is_empty(const list_t *lst)
 {
-	return (lst->head.next == NULL);
+    return (lst->head.next == NULL);
 }
 
 /**
@@ -104,8 +104,8 @@ int list_is_empty(const list_t *lst)
 inline
 void list_insert_after(list_node_t *x, list_node_t *t)
 {
-	t->next = x->next;
-	x->next = t;
+    t->next = x->next;
+    x->next = t;
 }
 
 /**
@@ -125,12 +125,13 @@ void list_insert_after(list_node_t *x, list_node_t *t)
 inline
 list_node_t *list_delete_after(list_node_t *x)
 {
-	list_node_t *t = x->next;
-	if (t != NULL)
-		x->next = t->next;	
-	return t;
+    list_node_t *t = x->next;
+    if (t != NULL)
+        x->next = t->next;    
+    return t;
 }
 
+// 删除链表头节点
 inline
 list_node_t *list_delete_head(list_t *lst)
 {
@@ -142,13 +143,13 @@ list_node_t *list_delete_head(list_t *lst)
 inline
 size_t list_size(const list_t *lst)
 {
-	size_t n = 0;
-	list_node_t *node = list_head(lst);
-	while (node != NULL) {
-		node = node->next;
-		n++;
-	}
-	return n;
+    size_t n = 0;
+    list_node_t *node = list_head(lst);
+    while (node != NULL) {
+        node = node->next;
+        n++;
+    }
+    return n;
 }
 
 // 交换两个链表
@@ -187,7 +188,7 @@ void list_reverse_after(list_node_t *x)
     if (x == NULL || x->next == NULL)
         return;
 
-	list_node_t *y = x->next;
+    list_node_t *y = x->next;
     while (y->next != NULL) {
         list_node_t *t = list_delete_after(y);
         list_insert_after(x, t);
---
+++ forward_list/recipe-01/samples/sample_constructor2.cpp
@@ -0,0 +1,36 @@
+// forward_list constructors
+#include <iostream>
+#include "forward_list.hpp"
+
+int main ()
+{
+  // constructors used in the same order as described above:
+
+  mini_stl::forward_list<int> first;                      // default: empty
+  mini_stl::forward_list<int> second (3,77);              // fill: 3 seventy-sevens
+  mini_stl::forward_list<int> third (second.begin(), second.end()); // range initialization
+  mini_stl::forward_list<int> fourth (third);            // copy constructor
+  mini_stl::forward_list<int> fifth (std::move(fourth));  // move ctor. (fourth wasted)
+  mini_stl::forward_list<int> sixth = {3, 52, 25, 90};    // initializer_list constructor
+
+  std::cout << "first:" ; for (int& x: first)  std::cout << " " << x; std::cout << '\n';
+  std::cout << "second:"; for (int& x: second) std::cout << " " << x; std::cout << '\n';
+  std::cout << "third:";  for (int& x: third)  std::cout << " " << x; std::cout << '\n';
+  std::cout << "fourth:"; for (int& x: fourth) std::cout << " " << x; std::cout << '\n';
+  std::cout << "fifth:";  for (int& x: fifth)  std::cout << " " << x; std::cout << '\n';
+  std::cout << "sixth:";  for (int& x: sixth)  std::cout << " " << x; std::cout << '\n';
+
+  return 0;
+}
+
+/*
+Possible output:
+
+forward_list constructor examples:
+first:
+second: 77 77 77
+third: 77 77 77
+fourth:
+fifth: 77 77 77
+sixth: 3 52 25 90
+*/
