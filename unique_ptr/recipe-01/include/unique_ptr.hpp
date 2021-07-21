53b9c826a8dd4bf644710fdc93bc780627d6f4b0
hexu <hexu_1985@sina.com>
2021-07-21 17:42:33 +0800

    调整unique_ptr代码风格

--- unique_ptr/recipe-01/include/unique_ptr.hpp
+++ unique_ptr/recipe-01/include/unique_ptr.hpp
@@ -14,7 +14,7 @@ struct default_delete {
     default_delete() = default;
     ~default_delete() = default;
 
-    void operator ()(T *p) const
+    void operator() (T* p) const
     {
         delete p;
     }
@@ -25,17 +25,17 @@ struct default_delete {
     default_delete() = default;
     ~default_delete() = default;
 
-    default_delete(const default_delete &d) {}
-    default_delete(default_delete &&d) {}
+    default_delete(const default_delete& d) {}
+    default_delete(default_delete&& d) {}
 
     template <typename U, 
-             typename = typename std::enable_if<std::is_convertible<U *, T *>::value>::type>
-    default_delete(const default_delete<U> &d) {}
+             typename = typename std::enable_if<std::is_convertible<U*, T*>::value>::type>
+    default_delete(const default_delete<U>& d) {}
 
-    default_delete &operator =(const default_delete &d) { return *this; }
-    default_delete &operator =(default_delete &&d) { return *this; }
+    default_delete& operator= (const default_delete& d) { return *this; }
+    default_delete& operator= (default_delete&& d) { return *this; }
 
-    void operator ()(T *p) const
+    void operator() (T* p) const
     {
         delete p;
     }
@@ -45,12 +45,12 @@ struct default_delete {
 template <typename T, typename Deleter=default_delete<T>>
 class unique_ptr {
 public:
-    using pointer = T *;
+    using pointer = T*;
     using element_type = T;
     using deleter_type = Deleter;
 
 private:
-    element_type *ptr_;
+    element_type* ptr_;
     Deleter del_;
 
     template <typename U, typename E>
@@ -61,28 +61,28 @@ public:
 
     unique_ptr(std::nullptr_t) noexcept: ptr_(nullptr), del_() {}
 
-    unique_ptr(T *p) noexcept: ptr_(p), del_() {}
+    unique_ptr(T* p) noexcept: ptr_(p), del_() {}
 
 #if 0
-    unique_ptr(T *p, Deleter &del): ptr_(p), del_(del) {}
+    unique_ptr(T* p, Deleter& del): ptr_(p), del_(del) {}
 
-    unique_ptr(T *p, Deleter &&del): ptr_(p), del_(std::move(del)) {}
+    unique_ptr(T* p, Deleter&& del): ptr_(p), del_(std::move(del)) {}
 #else
     unique_ptr(pointer p,
         typename std::conditional<std::is_reference<Deleter>::value, Deleter, const Deleter &>::type del): 
         ptr_(p), del_(del) {}
 
-    unique_ptr (pointer p, typename std::remove_reference<Deleter>::type &&del):
+    unique_ptr(pointer p, typename std::remove_reference<Deleter>::type&& del):
         ptr_(p), del_(std::move(del)) {}
 #endif
 
-    unique_ptr(unique_ptr &&u) noexcept: ptr_(u.ptr_), del_(std::forward<Deleter>(u.del_))
+    unique_ptr(unique_ptr&& u) noexcept: ptr_(u.ptr_), del_(std::forward<Deleter>(u.del_))
     {
         u.ptr_ = nullptr;
     };
 
-    template <typename U, typename E, typename = typename std::enable_if<std::is_convertible<U *, T *>::value>::type>
-    unique_ptr(unique_ptr<U, E> &&u) noexcept: ptr_(u.ptr_), del_(std::forward<E>(u.del_))
+    template <typename U, typename E, typename = typename std::enable_if<std::is_convertible<U*, T*>::value>::type>
+    unique_ptr(unique_ptr<U, E>&& u) noexcept: ptr_(u.ptr_), del_(std::forward<E>(u.del_))
     {
         u.ptr_ = nullptr;
     }
@@ -94,7 +94,7 @@ public:
         }
     }
 
-    unique_ptr &operator =(unique_ptr &&r) noexcept 
+    unique_ptr& operator= (unique_ptr&& r) noexcept 
     {
         if (this != &r) {
             reset(r.release());
@@ -104,23 +104,23 @@ public:
         return *this;
     }
 
-    T *release() noexcept 
+    T* release() noexcept 
     {
-        T *p = ptr_;
+        T* p = ptr_;
         ptr_ = nullptr;
         return p;
     }
 
-    void reset(T *p = nullptr) noexcept
+    void reset(T* p = nullptr) noexcept
     {
-        T *old_ptr = ptr_;
+        T* old_ptr = ptr_;
         ptr_ = p;
         if (old_ptr != nullptr) {
             del_(old_ptr);
         }
     }
 
-    void swap(unique_ptr &other) noexcept 
+    void swap(unique_ptr& other) noexcept 
     {
         if (this != &other) {
             using std::swap;
@@ -130,17 +130,17 @@ public:
         }
     }
 
-    T *get() const noexcept 
+    T* get() const noexcept 
     {
         return ptr_;
     }
 
-    T &operator *() const 
+    T& operator* () const 
     {
         return *ptr_;
     }
 
-    T *operator ->() const noexcept
+    T* operator-> () const noexcept
     {
         return ptr_;
     }
@@ -150,27 +150,26 @@ public:
         return ptr_ != nullptr;
     }
 
-    deleter_type &get_deleter() noexcept
+    deleter_type& get_deleter() noexcept
     {
         return del_;
     }
 
-    const deleter_type &get_deleter() const noexcept
+    const deleter_type& get_deleter() const noexcept
     {
         return del_;
     }
 };
 
 template <typename T, typename... Args>
-unique_ptr<T> make_unique(Args &&... args)
+unique_ptr<T> make_unique(Args&&... args)
 {
     return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template <typename charT, typename traits, typename T, typename Deleter>
-std::basic_ostream<charT, traits> &operator <<(
-        std::basic_ostream<charT, traits> &os, 
-        const unique_ptr<T, Deleter> &ptr)
+std::basic_ostream<charT, traits>& operator<< (
+        std::basic_ostream<charT, traits>& os, const unique_ptr<T, Deleter>& ptr)
 {
     os << ptr.get();
     return os;
@@ -178,65 +177,64 @@ std::basic_ostream<charT, traits> &operator <<(
 
 // comparaison operators
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator ==(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator== (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() == rhs.get());
 }
 
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator !=(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator!= (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() != rhs.get());
 }
 
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator <=(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator<= (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() <= rhs.get());
 }
 
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator <(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator< (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() < rhs.get());
 }
 
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator >=(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator>= (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() >= rhs.get());
 }
 
 template <typename T1, typename D1, typename T2, typename D2> 
-inline bool operator>(const unique_ptr<T1, D1> &lhs, const unique_ptr<T2, D2> &rhs) noexcept
+inline bool operator> (const unique_ptr<T1, D1>& lhs, const unique_ptr<T2, D2>& rhs) noexcept
 {
     return (lhs.get() > rhs.get());
 }
 
 template <typename T, typename D> 
-inline bool operator ==(const unique_ptr<T, D> &lhs, std::nullptr_t) noexcept
+inline bool operator== (const unique_ptr<T, D>& lhs, std::nullptr_t) noexcept
 {
     return !lhs;
 }
 
 template <typename T, typename D> 
-inline bool operator ==(std::nullptr_t, const unique_ptr<T, D> &rhs) noexcept
+inline bool operator== (std::nullptr_t, const unique_ptr<T, D>& rhs) noexcept
 {
     return !rhs;
 }
 
 template <typename T, typename D> 
-inline bool operator !=(const unique_ptr<T, D> &lhs, std::nullptr_t) noexcept
+inline bool operator!= (const unique_ptr<T, D>& lhs, std::nullptr_t) noexcept
 {
     return (bool) lhs;
 }
 
 template <typename T, typename D> 
-inline bool operator !=(std::nullptr_t, const unique_ptr<T, D> &rhs) noexcept
+inline bool operator!= (std::nullptr_t, const unique_ptr<T, D>& rhs) noexcept
 {
     return (bool) rhs;
 }
-
 
 }   // namespace mini_stl
 
@@ -244,7 +242,7 @@ namespace std {
 
 template <typename T, typename Deleter>
 struct hash<mini_stl::unique_ptr<T, Deleter>> {
-    size_t operator ()(const mini_stl::unique_ptr<T, Deleter> &p)
+    size_t operator() (const mini_stl::unique_ptr<T, Deleter>& p)
     {
         return std::hash<typename mini_stl::unique_ptr<T, Deleter>::pointer>()(p.get());
     }
--- unique_ptr/recipe-02/include/unique_ptr_array.tcc
+++ unique_ptr/recipe-02/include/unique_ptr_array.tcc
@@ -1,31 +1,31 @@
 #if 0
 template <typename T>
-struct default_delete<T []> {
+struct default_delete<T[]> {
     default_delete() = default;
     ~default_delete() = default;
 
-    void operator ()(T *p) const
+    void operator() (T* p) const
     {
         delete [] p;
     }
 };
 #else
 template <typename T>
-struct default_delete<T []> {
+struct default_delete<T[]> {
     default_delete() = default;
     ~default_delete() = default;
 
-    default_delete(const default_delete &d) {}
-    default_delete(default_delete &&d) {}
+    default_delete(const default_delete& d) {}
+    default_delete(default_delete&& d) {}
 
     template <typename U, 
              typename = typename std::enable_if<std::is_convertible<U, T>::value>::type>
-    default_delete(const default_delete<U> &d) {}
+    default_delete(const default_delete<U>& d) {}
 
-    default_delete &operator =(const default_delete &d) { return *this; }
-    default_delete &operator =(default_delete &&d) { return *this; }
+    default_delete& operator= (const default_delete& d) { return *this; }
+    default_delete& operator= (default_delete&& d) { return *this; }
 
-    void operator ()(T *p) const
+    void operator() (T* p) const
     {
         delete [] p;
     }
@@ -35,12 +35,12 @@ struct default_delete<T []> {
 template<typename T, typename Deleter>
 class unique_ptr<T[], Deleter> {
 public:
-    using pointer = T *;
+    using pointer = T*;
     using element_type = T;
     using deleter_type = Deleter;
 
 private:
-    element_type *ptr_;
+    element_type* ptr_;
     Deleter del_;
 
     template <typename U, typename E>
@@ -51,28 +51,28 @@ public:
 
     unique_ptr(std::nullptr_t) noexcept: ptr_(nullptr), del_() {}
 
-    unique_ptr(T *p) noexcept: ptr_(p), del_() {}
+    unique_ptr(T* p) noexcept: ptr_(p), del_() {}
 
 #if 0
-    unique_ptr(T *p, Deleter &del): ptr_(p), del_(del) {}
+    unique_ptr(T* p, Deleter& del): ptr_(p), del_(del) {}
 
-    unique_ptr(T *p, Deleter &&del): ptr_(p), del_(std::move(del)) {}
+    unique_ptr(T* p, Deleter&& del): ptr_(p), del_(std::move(del)) {}
 #else
     unique_ptr(pointer p,
-        typename std::conditional<std::is_reference<Deleter>::value, Deleter, const Deleter &>::type del): 
+        typename std::conditional<std::is_reference<Deleter>::value, Deleter, const Deleter&>::type del): 
         ptr_(p), del_(del) {}
 
-    unique_ptr (pointer p, typename std::remove_reference<Deleter>::type &&del):
+    unique_ptr(pointer p, typename std::remove_reference<Deleter>::type&& del):
         ptr_(p), del_(std::move(del)) {}
 #endif
 
-    unique_ptr(unique_ptr &&u) noexcept: ptr_(u.ptr_), del_(std::forward<Deleter>(u.del_))
+    unique_ptr(unique_ptr&& u) noexcept: ptr_(u.ptr_), del_(std::forward<Deleter>(u.del_))
     {
         u.ptr_ = nullptr;
     };
 
-    template<typename U, typename E, typename = typename std::enable_if<std::is_convertible<U *, T *>::value>::type>
-    unique_ptr(unique_ptr<U, E> &&u) noexcept: ptr_(u.ptr_), del_(std::forward<E>(u.del_))
+    template<typename U, typename E, typename = typename std::enable_if<std::is_convertible<U*, T*>::value>::type>
+    unique_ptr(unique_ptr<U, E>&& u) noexcept: ptr_(u.ptr_), del_(std::forward<E>(u.del_))
     {
         u.ptr_ = nullptr;
     }
@@ -84,7 +84,7 @@ public:
         }
     }
 
-    unique_ptr &operator =(unique_ptr &&r) noexcept 
+    unique_ptr& operator= (unique_ptr&& r) noexcept 
     {
         if (this != &r) {
             reset(r.release());
@@ -94,23 +94,23 @@ public:
         return *this;
     }
 
-    T *release() noexcept 
+    T* release() noexcept 
     {
-        T *p = ptr_;
+        T* p = ptr_;
         ptr_ = nullptr;
         return p;
     }
 
-    void reset(T *p = nullptr) noexcept
+    void reset(T* p = nullptr) noexcept
     {
-        T *old_ptr = ptr_;
+        T* old_ptr = ptr_;
         ptr_ = p;
         if (old_ptr != nullptr) {
             del_(old_ptr);
         }
     }
 
-    void swap(unique_ptr &other) noexcept 
+    void swap(unique_ptr& other) noexcept 
     {
         if (this != &other) {
             using std::swap;
@@ -120,12 +120,12 @@ public:
         }
     }
 
-    T *get() const noexcept 
+    T* get() const noexcept 
     {
         return ptr_;
     }
 
-    T &operator [](size_t i) const
+    T& operator[] (size_t i) const
     {
         return ptr_[i];
     }
@@ -135,12 +135,12 @@ public:
         return ptr_ != nullptr;
     }
 
-    deleter_type &get_deleter() noexcept
+    deleter_type& get_deleter() noexcept
     {
         return del_;
     }
 
-    const deleter_type &get_deleter() const noexcept
+    const deleter_type& get_deleter() const noexcept
     {
         return del_;
     }
