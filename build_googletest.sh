e880dc4d89f874ea0fcee14a96f3660d852807b8
hexu <hexu_1985@sina.com>
2021-08-17 11:17:22 +0800

    update build_googletest.sh

--- build_googletest.sh
+++ build_googletest.sh
@@ -1,5 +1,2 @@
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/local \
-    ..
-cmake --build . --target install
+cmake -H. -Bbuild -DCMAKE_INSTALL_PREFIX=~/local 
+cmake --build build --target install
