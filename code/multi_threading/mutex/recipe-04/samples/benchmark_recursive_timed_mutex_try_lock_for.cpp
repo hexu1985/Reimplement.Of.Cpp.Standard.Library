648c65cd8ad238c394c9c7df2c90d1b7386f711d
hexu <hexu_1985@sina.com>
2022-05-06 17:03:26 +0800

    add samples of mutex/recipe-04

---
+++ code/multi_threading/mutex/recipe-04/samples/benchmark_recursive_timed_mutex_try_lock_for.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <sstream>
+
+using namespace std::chrono;
+ 
+std::mutex cout_mutex; // control access to std::cout
+std::recursive_timed_mutex mutex;
+ 
+void job(int id)
+{
+    std::ostringstream stream;
+ 
+    for (int i = 0; i < 3; ++i) {
+        if (mutex.try_lock_for(milliseconds(100))) {
+            stream << "success ";
+            std::this_thread::sleep_for(milliseconds(100));
+            mutex.unlock();
+        } else {
+            stream << "failed ";
+        }
+        std::this_thread::sleep_for(milliseconds(100));
+    }
+ 
+    std::lock_guard<std::mutex> lock{cout_mutex};
+    std::cout << "[" << id << "] " << stream.str() << "\n";
+}
+ 
+int main()
+{
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 4; ++i) {
+        threads.emplace_back(job, i);
+    }
+ 
+    for (auto& i: threads) {
+        i.join();
+    }
+}
