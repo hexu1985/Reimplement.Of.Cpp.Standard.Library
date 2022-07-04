8a1f4440bfd18a889a59b9ae7183cc08f49908c4
hexu <hexu_1985@sina.com>
2022-07-04 18:07:47 +0800

    add utility/integer_sequence

---
+++ code/utility/integer_sequence/recipe-01/include/integer_sequence.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+
+namespace mini_stl {
+
+template <typename T, T... Ints> struct integer_sequence {
+    static constexpr std::size_t size() noexcept { return sizeof...(Ints); }
+};
+
+template <std::size_t... Ints> 
+using index_sequence = integer_sequence<std::size_t, Ints...>;
+
+template <typename T, T N, typename = void>
+struct make_integer_sequence_helper {
+    template <typename>
+    struct unfold;
+
+    template <T... Prev>
+    struct unfold<integer_sequence<T, Prev...>> {
+        using type = integer_sequence<T, Prev..., N-1>;
+    };
+
+    using type = typename unfold<typename make_integer_sequence_helper<T, N-1>::type>::type;
+};
+
+template <typename T, T N>
+struct make_integer_sequence_helper<T, N, typename std::enable_if<N==0>::type> {
+    using type = integer_sequence<T>;
+};
+
+template <typename T, T N>
+using make_integer_sequence = typename make_integer_sequence_helper<T, N>::type;
+
+template <std::size_t N>
+using make_index_sequence = make_integer_sequence<std::size_t, N>;
+
+template<class... T>
+using index_sequence_for = make_index_sequence<sizeof...(T)>;
+
+}   // namespace mini_stl
+
---
+++ code/utility/integer_sequence/recipe-01/samples/sample_integer_sequence.cpp
@@ -0,0 +1,62 @@
+#include <tuple>
+#include <iostream>
+#include <array>
+#include "integer_sequence.hpp"
+ 
+// debugging aid
+template<typename T, T... ints>
+void print_sequence(mini_stl::integer_sequence<T, ints...> int_seq)
+{
+    std::cout << "The sequence of size " << int_seq.size() << ": ";
+    ((std::cout << ints << ' '), ...);
+    std::cout << '\n';
+}
+ 
+// convert array into a tuple
+template<typename Array, std::size_t... I>
+auto a2t_impl(const Array& a, mini_stl::index_sequence<I...>)
+{
+    return std::make_tuple(a[I]...);
+}
+ 
+template<typename T, std::size_t N, typename Indices = mini_stl::make_index_sequence<N>>
+auto a2t(const std::array<T, N>& a)
+{
+    return a2t_impl(a, Indices{});
+}
+ 
+// pretty-print a tuple
+template<class Ch, class Tr, class Tuple, std::size_t... Is>
+void print_tuple_impl(std::basic_ostream<Ch,Tr>& os,
+                      const Tuple& t,
+                      mini_stl::index_sequence<Is...>)
+{
+    ((os << (Is == 0? "" : ", ") << std::get<Is>(t)), ...);
+}
+ 
+template<class Ch, class Tr, class... Args>
+auto& operator<<(std::basic_ostream<Ch, Tr>& os,
+                 const std::tuple<Args...>& t)
+{
+    os << "(";
+    print_tuple_impl(os, t, mini_stl::index_sequence_for<Args...>{});
+    return os << ")";
+}
+ 
+int main()
+{
+    print_sequence(mini_stl::integer_sequence<unsigned, 9, 2, 5, 1, 9, 1, 6>{});
+    print_sequence(mini_stl::make_integer_sequence<int, 20>{});
+    print_sequence(mini_stl::make_index_sequence<10>{});
+    print_sequence(mini_stl::index_sequence_for<float, std::iostream, char>{});
+ 
+    std::array<int, 4> array = {1, 2, 3, 4};
+ 
+    // convert an array into a tuple
+    auto tuple = a2t(array);
+    static_assert(std::is_same_v<decltype(tuple),
+                                 std::tuple<int, int, int, int>>, "");
+ 
+    // print it to cout
+    std::cout << "The tuple: " << tuple << '\n';
+}
