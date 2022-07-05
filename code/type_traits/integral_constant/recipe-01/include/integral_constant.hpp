00b4a5bf90d64db012eed71f188ee9ed32d7654f
hexu <hexu_1985@sina.com>
2022-07-05 16:27:04 +0800

    update integral_constant/recipe-01/include/integral_constant.hpp

--- code/type_traits/integral_constant/recipe-01/include/integral_constant.hpp
+++ code/type_traits/integral_constant/recipe-01/include/integral_constant.hpp
@@ -5,7 +5,9 @@ namespace mini_stl {
 template <typename T, T v>
 struct integral_constant {
     static constexpr T value = v;
+    // typedef T value_type;
     using value_type = T;
+    // typedef integral_constant<T,v> type;
     using type = integral_constant; // using injected-class-name
     constexpr operator value_type() const noexcept { return value; }
     constexpr value_type operator()() const noexcept { return value; } // since c++14
@@ -16,6 +18,5 @@ using true_type = integral_constant<bool,true>;
 
 //typedef integral_constant<bool,false> false_type;
 using false_type = integral_constant<bool,false>;
-
 
 }   // namespace mini_stl
