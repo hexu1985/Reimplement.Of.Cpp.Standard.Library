700cac5876c94bb6d7cb30bfd5a8fcfa2ac7a757
hexu <hexu_1985@sina.com>
2022-07-06 18:36:41 +0800

    add type_traits/enable_if

---
+++ code/type_traits/enable_if/recipe-01/include/enable_if.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Hx {
+
+template<bool B, class T = void>
+struct enable_if {};
+ 
+template<class T>
+struct enable_if<true, T> { typedef T type; };
+
+template< bool B, class T = void >
+using enable_if_t = typename enable_if<B,T>::type;
+
+}   // namespace Hx
