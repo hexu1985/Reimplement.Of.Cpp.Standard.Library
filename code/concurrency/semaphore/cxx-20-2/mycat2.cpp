56d7660d0971fbfa56c4cb2b155663166b41c6b7
hexu <hexu_1985@sina.com>
2024-08-16 17:27:45 +0800

    add code/concurrency/semaphore/cxx-20-2

---
+++ code/concurrency/semaphore/cxx-20-2/mycat2.cpp
@@ -0,0 +1,107 @@
+#include <thread>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore>
+
+using std::counting_semaphore;
+using std::binary_semaphore;
+
+#define	BUFFSIZE		8192
+#define	NBUFF	 8
+
+struct {	/* data shared by producer and consumer */
+  struct {
+    char	data[BUFFSIZE];			/* a buffer */
+    ssize_t	n;						/* count of #bytes in the buffer */
+  } buff[NBUFF];					/* NBUFF of these buffers/counts */
+  binary_semaphore *mutex;
+  counting_semaphore<> *nempty, *nstored;
+} shared;
+
+FILE* fp;							/* input file to copy to stdout */
+void produce();
+void consume();
+
+int
+main(int argc, char **argv)
+{
+    std::thread thr_produce, thr_consume;
+
+	if (argc != 2) {
+		printf("usage: mycat2 <pathname>\n");
+        exit(1);
+    }
+
+	fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("open error for %s\n", argv[1]);
+        exit(2);
+    }
+
+    setbuf(fp, NULL);
+
+		/* 4initialize three semaphores */
+	shared.mutex = new binary_semaphore{1};
+	shared.nempty = new counting_semaphore<>{NBUFF};
+	shared.nstored = new counting_semaphore<>{0};
+
+		/* 4one producer thread, one consumer thread */
+    thr_produce = std::thread(produce);
+    thr_consume = std::thread(consume);
+
+    thr_produce.join();
+    thr_consume.join();
+
+    delete shared.mutex;
+    delete shared.nempty;
+    delete shared.nstored;
+	exit(0);
+}
+/* end main */
+
+/* include prodcons */
+void produce()
+{
+	int		i;
+
+	for (i = 0; ; ) {
+		shared.nempty->acquire();	/* wait for at least 1 empty slot */
+
+		shared.mutex->acquire();
+			/* 4critical region */
+		shared.mutex->release();
+
+		shared.buff[i].n = fread(shared.buff[i].data, 1, BUFFSIZE, fp);
+		if (shared.buff[i].n == 0) {
+		    shared.nstored->release();	/* 1 more stored item */
+			return;
+		}
+		if (++i >= NBUFF)
+			i = 0;					/* circular buffer */
+
+		shared.nstored->release();	/* 1 more stored item */
+	}
+}
+
+void consume()
+{
+	int		i;
+
+	for (i = 0; ; ) {
+		shared.nstored->acquire();		/* wait for at least 1 stored item */
+
+		shared.mutex->acquire();
+			/* 4critical region */
+		shared.mutex->release();
+
+		if (shared.buff[i].n == 0)
+			return;
+		fwrite(shared.buff[i].data, 1, shared.buff[i].n, stdout);
+		if (++i >= NBUFF)
+			i = 0;					/* circular buffer */
+
+		shared.nempty->release();		/* 1 more empty slot */
+	}
+}
+/* end prodcons */
---
+++ code/concurrency/semaphore/cxx-20-2/prodcons4.cpp
@@ -0,0 +1,127 @@
+#include <thread>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore>
+
+using std::counting_semaphore;
+using std::binary_semaphore;
+
+#define	NBUFF	 	 10
+#define	MAXNTHREADS	100
+
+int		nitems, nproducers, nconsumers;		/* read-only */
+
+struct {	/* data shared by producers and consumers */
+  int	buff[NBUFF];
+  int	nput;			/* item number: 0, 1, 2, ... */
+  int	nputval;		/* value to store in buff[] */
+  int	nget;			/* item number: 0, 1, 2, ... */
+  int	ngetval;		/* value fetched from buff[] */
+  binary_semaphore *mutex;
+  counting_semaphore<> *nempty, *nstored;
+} shared;
+
+void produce(int* arg);
+void consume(int* arg);
+/* end globals */
+
+/* include main */
+int
+main(int argc, char **argv)
+{
+	int		i, prodcount[MAXNTHREADS], conscount[MAXNTHREADS];
+    std::thread thr_produce[MAXNTHREADS], thr_consume[MAXNTHREADS];
+
+	if (argc != 4) {
+		printf("usage: prodcons4 <#items> <#producers> <#consumers>\n");
+        exit(1);
+    }
+	nitems = atoi(argv[1]);
+	nproducers = std::min(atoi(argv[2]), MAXNTHREADS);
+	nconsumers = std::min(atoi(argv[3]), MAXNTHREADS);
+
+		/* 4initialize three semaphores */
+	shared.mutex = new binary_semaphore{1};
+	shared.nempty = new counting_semaphore<>{NBUFF};
+	shared.nstored = new counting_semaphore<>{0};
+
+		/* 4create all producers and all consumers */
+	for (i = 0; i < nproducers; i++) {
+		prodcount[i] = 0;
+        thr_produce[i] = std::thread(produce, &prodcount[i]);
+	}
+	for (i = 0; i < nconsumers; i++) {
+		conscount[i] = 0;
+        thr_consume[i] = std::thread(consume, &conscount[i]);
+	}
+
+		/* 4wait for all producers and all consumers */
+	for (i = 0; i < nproducers; i++) {
+        thr_produce[i].join();
+		printf("producer count[%d] = %d\n", i, prodcount[i]);	
+	}
+	for (i = 0; i < nconsumers; i++) {
+        thr_consume[i].join();
+		printf("consumer count[%d] = %d\n", i, conscount[i]);	
+	}
+
+    delete shared.mutex;
+    delete shared.nempty;
+    delete shared.nstored;
+	exit(0);
+}
+/* end main */
+
+/* include produce */
+void produce(int* arg)
+{
+	for ( ; ; ) {
+		shared.nempty->acquire();	/* wait for at least 1 empty slot */
+		shared.mutex->acquire();
+
+		if (shared.nput >= nitems) {
+            shared.nstored->release();	/* let consumers terminate */
+            shared.nempty->release();
+            shared.mutex->release();
+			return;			/* all done */
+		}
+
+		shared.buff[shared.nput % NBUFF] = shared.nputval;
+		shared.nput++;
+		shared.nputval++;
+
+		shared.mutex->release();
+		shared.nstored->release();	/* 1 more stored item */
+		*arg += 1;
+	}
+}
+/* end produce */
+
+/* include consume */
+void consume(int* arg)
+{
+	int		i;
+
+	for ( ; ; ) {
+		shared.nstored->acquire();		/* wait for at least 1 stored item */
+		shared.mutex->acquire();
+
+		if (shared.nget >= nitems) {
+            shared.nstored->release();
+            shared.mutex->release();
+			return;			/* all done */
+		}
+
+		i = shared.nget % NBUFF;
+		if (shared.buff[i] != shared.ngetval)
+			printf("error: buff[%d] = %d\n", i, shared.buff[i]);
+		shared.nget++;
+		shared.ngetval++;
+
+		shared.mutex->release();
+		shared.nempty->release();		/* 1 more empty slot */
+		*arg += 1;
+	}
+}
+/* end consume */
